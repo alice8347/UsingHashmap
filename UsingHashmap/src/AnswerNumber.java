e4f34aedcbc9f9823ec82bc6cc714fb8a6a714ed
alice8347 <alice8347@hotmail.com>
2015-08-28 09:29:50 -0400

    Commit the whole project

--- UsingHashmap/src/AnswerNumber.java
+++ UsingHashmap/src/AnswerNumber.java
@@ -1,37 +1,63 @@
 import java.util.Scanner;
 import java.util.HashMap;
+import java.io.*;
 
 public class AnswerNumber {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		Scanner sc = new Scanner(System.in);
-		HashMap<Integer, String> myMap = new HashMap<Integer, String>();
-		int num = 0;
+		HashMap<String, String> myMap = new HashMap<String, String>();
+		
+		String fileName = (System.getProperty("user.dir") + File.separatorChar + "myFile.txt");
+
+		String num = "";
 		String word = "";
 		String choice = "Y";
 		
-		myMap.put(0, "zero");
-		myMap.put(1, "one");
-		myMap.put(2, "two");
-		myMap.put(3, "three");
-		myMap.put(4, "four");
-		
-		while (choice.equalsIgnoreCase("Y")) {
-			System.out.print("Prompt: Enter a number: ");
-			num = sc.nextInt();
-			if (myMap.containsKey(num)) {
-				System.out.println("Response: You entered " + myMap.get(num) + ".");
-			} else {
-				System.out.print("The number is not yet in the map. Please type it in English. ");
-				word = sc.next();
-				sc.nextLine();
-				myMap.put(num, word);
-				System.out.println("Response: You entered " + myMap.get(num) + ".");
-			}
-			System.out.print("Would you like to try again? Y/N ");
-			choice = sc.next();
-			sc.nextLine();
-			System.out.println();
+		try {
+			while (choice.equalsIgnoreCase("Y")) {
+				BufferedReader reader = new BufferedReader(new FileReader(fileName));
+				
+				String readWholeLine = reader.readLine();
+
+				while (readWholeLine != null) {
+					String line[] = new String[2];
+					line = readWholeLine.split("\t");
+					String key = line[0];
+					String value = line[1];
+					myMap.put(key, value);
+					readWholeLine = reader.readLine();
+				}
+				reader.close();
+	
+	 			System.out.print("Prompt: Enter a number: ");
+	 			num = sc.nextLine();
+	 			if (myMap.containsKey(num)) {
+	 				System.out.println("Response: You entered " + myMap.get(num) + ".");
+	 			} else {
+	 				System.out.print("The number is not yet in the map. Please type it in English. ");
+	 				word = sc.nextLine();
+	 				myMap.put(num, word);
+	 				System.out.println("Response: You entered " + myMap.get(num) + ".");
+	 				
+	 				PrintWriter writer = new PrintWriter(new File(fileName));
+	 				for (String key : myMap.keySet()) {
+	 					writer.println(key + "\t" + myMap.get(key));
+	 				}
+	 				writer.close();
+	 			}
+	 			System.out.print("Would you like to try again? Y/N ");
+	 			choice = sc.next();
+	 			sc.nextLine();
+	 			System.out.println();
+	 		}
+		} catch (FileNotFoundException e) {
+			System.out.println(e.getMessage());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		} catch (NullPointerException e) {
+			System.out.println(e.getMessage());
+		} finally {
+			System.out.println("Thank you for using the App.");
 		}
-		System.out.println("Thank you for using the App.");
 	}
 }
